ea6167f5e761b2e98acff0f886880faeac2b7b88
prasanna <prasannatwenty@gmail.com>
2024-11-05 11:15:32 +0530

    modified

--- agents.py
+++ agents.py
@@ -1,3 +1,5 @@
+import re
+import anthropic
 from ultralyticsplus import YOLO
 from PIL import ImageDraw 
 from io import BytesIO
@@ -9,27 +11,27 @@ class OBBModule():
         #   - yolov11x-2c.pt
         #   - yolov8n-2c-v2.pt
         #   - yolov8n-2c.pt
-        self.model = YOLO('3C-models/18102024.pt')
+        #self.model = YOLO('C:\\Users\\LAU\\Downloads\\FASTAPI3\\FASTAPI\\18102024.pt').to('cpu')
+        self.model = YOLO("21102024.pt").to('cpu')
         # set model parameters
         self.model.overrides['conf'] = 0.35  # NMS confidence threshold
         self.model.overrides['iou'] = 0.45  # NMS IoU threshold
         self.model.overrides['agnostic_nms'] = False  # NMS class-agnostic
         self.model.overrides['max_det'] = 1000  # maximum number of detections per image
 
-    def detect_bbox(self, img):
-        print("###########################")
-        print("RUNNING>>>>")
-        print("###########################")
-        if img.mode != "RGB":
-            img = img.convert("RGB")
+    def detect_bbox(self,img1, img2=None):
+        if img1.mode != "RGB":
+            img1 = img1.convert("RGB")
         
-        # Predict bboxes on the 125 DPI image
-        results = self.model.predict(img)
-        print(results[0].boxes)
-        print("PREDICTED>>>>")
-        cropped_images = []
-        
-        base_img = img.copy()
+        results = self.model.predict(img1)
+        print(results[0].boxes.xyxy)
+
+        if img2 is not None:
+            scale_factor_x = img2.width / img1.width
+            scale_factor_y = img2.height / img1.height
+        else:
+            img2 = img1
+            scale_factor_x = scale_factor_y = 1
 
         obb_data = []
 
@@ -39,47 +41,80 @@ class OBBModule():
             results[0].boxes.xywh.tolist(),
             results[0].boxes.cls.tolist()
         ):
-            if conf > 0.26:
-                bbox = table_bbox_xyxy.tolist()
-                
-                x1, y1, x2, y2 = bbox
-                draw = ImageDraw.Draw(img)
-                draw.rectangle([x1, y1, x2, y2], outline="red", width=3)
-                
-                if class_id == 0:
-                    tbl_cls = "empty"
-                elif class_id == 1:
-                    tbl_cls = "normal"
-                elif class_id == 2:
-                    tbl_cls = "tilted"
+            # if conf > 0.26:
+            bbox = table_bbox_xyxy.tolist()
+            
+            x1, y1, x2, y2 = [coord * scale for coord, scale in zip(bbox[:4], [scale_factor_x, scale_factor_y, scale_factor_x, scale_factor_y])]
+            x, y, w, h = table_bbox_xywh
+            x, y, w, h = [coord * scale for coord, scale in zip([x, y, w, h], [scale_factor_x, scale_factor_y, scale_factor_x, scale_factor_y])]
+            obb_data.append({
+                "class_id": class_id,
+                "xyxy": [x1, y1, x2, y2],
+                "xywh": [x, y, w, h]
+            })
+            print(">>>>>> initial bbox", [x1, y1, x2, y2])
 
-                draw.text((x1, y1), tbl_cls, fill="red")
-
-                cropped_images.append([class_id, base_img.crop((x1, y1, x2, y2))])
-
-                xc, yc, w, h = table_bbox_xywh
-
-                obb_data.append({
-                    "class": tbl_cls,
-                    "xyxy": [x1, y1, x2, y2],
-                    "xywh": [xc, yc, w, h],
-                })
-
-            else:
-                obb_data.append({
-                    "class": None,
-                    "xyxy": [0, 0, 0, 0],
-                    "xywh": [xc, yc, w, h],
-                })
+            # else:
+            #     obb_data.append({
+            #         "class_id": None,
+            #         "xyxy": None,
+            #         "xywh": None
+            #     })
         
         buffered = BytesIO()
-        base_img.save(buffered, format="PNG")
+        img2.save(buffered, format="PNG")
         base_img_string = base64.b64encode(buffered.getvalue()).decode('utf-8')
         
         return {
             "bbox_data": obb_data,
             "actual_image": base_img_string,
-            "height": base_img.height,
-            "width": base_img.width,
+            "height": img2.height,
+            "width": img2.width,
             "num_tables": len(results[0].boxes.xyxy),
         }
+
+class TOCRAgent:
+    def __init__(self, system_prompt) -> None:
+
+        self.client = anthropic.Anthropic()
+
+        self.system_prompt = system_prompt
+
+    def extract_code(self, content):
+        code_blocks = re.findall(r'<final>\n<table(.*?)</final>', content, re.DOTALL)
+        return code_blocks
+
+    def extract_table(self, base64_image):
+        msg = []
+        msg.append(
+            {
+                'role': 'user',
+                'content': [
+                    {
+                    "type": "text",
+                    "text": "Extract the table step by step."
+                    },
+                    {
+                    "type": "image",
+                    "source": {
+                        "type": "base64",
+                        "media_type": "image/png",
+                        "data": base64_image,
+                    },
+                    }
+                ]
+            }
+        )
+    
+        response = self.client.messages.create(
+            model="claude-3-5-sonnet-latest",
+            messages=msg,
+            max_tokens=8192,
+            system=self.system_prompt,
+            extra_headers={
+                'anthropic-beta': 'max-tokens-3-5-sonnet-2024-07-15'
+            },
+            temperature=0,
+        )
+        print(response.content[0].text)
+        return self.extract_code(response.content[0].text), response.usage
--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+import json
+import os
+import shutil
+import tempfile
+from fastapi import Body, FastAPI, UploadFile, File, Form, HTTPException
+from fastapi.responses import FileResponse
+import pandas as pd
 from pydantic import BaseModel, Field
 from typing import List
 import io
 import pdfplumber
 from PIL import Image
 import base64
-from agents import OBBModule
+from agents import OBBModule, TOCRAgent
 from fastapi.middleware.cors import CORSMiddleware
 from io import BytesIO
+from utils import convert_htm_to_excel
+from openpyxl.drawing.image import Image as ExcelImage
+from dotenv import load_dotenv
+from starlette.background import BackgroundTask
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -19,6 +31,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+agent = TOCRAgent(system_prompt=open("./system_prompt.txt", 'r').read())
 obb = OBBModule()
 
 def get_pil_image(image):
@@ -29,7 +42,7 @@ def is_table_empty(table):
 
 @app.post("/categorize")
 async def categorize(
-    selected_pages: str = Form(...),
+    selected_pages: str = Form(...), # this should a string like: '2,3,5-8'
     pdf_file: UploadFile = File(...)
 ):
     try:
@@ -37,25 +50,25 @@ async def categorize(
     except Exception as e:
         raise HTTPException(status_code=400, detail="Error reading the PDF file")
     
-    selected_pages_ = []
+    selected_pages_list = []
     for i in selected_pages.split(","):
-        selected_pages_.append(int(i))
+        selected_pages_list.append(int(i))
 
-    selected_pages = selected_pages_
 
     response = []
     with pdfplumber.open(io.BytesIO(pdf_bytes)) as pdf:
-        for i, page_num in enumerate(selected_pages):
-            if page_num >= len(pdf.pages):
+        for page_num in selected_pages_list:
+            page_index = page_num - 1  # Adjust for zero-based indexing
+            if page_index < 0 or page_index >= len(pdf.pages):
                 raise HTTPException(status_code=400, detail=f"Page number {page_num} out of range for the provided PDF")
 
-            page = pdf.pages[page_num - 1]
-            pix = page.to_image()
-            pix.save("img.png")
-            img = Image.open('img.png')
+            page = pdf.pages[page_index]
+            # pix = page.to_image()
+            # pix.save("img.png")
+            # img = Image.open('img.png')
             # img1, img2 = page.to_image().original, page.to_image(resolution=275).original
-            img = page.to_image().original
-            obb_result = obb.detect_bbox(img)
+            img1, img2 = page.to_image().original, page.to_image(resolution=275).original 
+            obb_result = obb.detect_bbox(img1, img2)
 
             if obb_result['num_tables'] > 0:
                 width, height = page.width, page.height
@@ -74,24 +87,27 @@ async def categorize(
             else:
                 category = "Edge Case"
 
-            # Category Adjustment if 'Word'
-            if category == 'Word':
-                for cls, img in img:
-                    if cls == 2:
-                        category = "Scanned"
+                if category == 'Word':
+                    for cls_id, cropped_img in obb_result.get('cropped_images', []):
+                     if cls_id == 2:
+                         category = "Scanned"
+                         break
 
             response.append({
                 "page_num": page_num,
                 "category": category,
-                "bbox": obb_result if category != 'Word' else None,
+                # "bbox": obb_result if category != 'Word' else None,
+                "bbox": obb_result,
                 "dpi": 275,
             })
-
+    with open("cat_bbox.txt", 'w') as f:
+        f.write(str(response))
     return response
 
 @app.post("/set_dpi")
 async def set_dpi(
     dpi: int = Form(...),
+    bbox: str = Form(...),
     page_num: int = Form(...),
     pdf_file: UploadFile = File(...)
 ):
@@ -99,26 +115,189 @@ async def set_dpi(
         pdf_bytes = await pdf_file.read()
     except Exception as e:
         raise HTTPException(status_code=400, detail="Error reading the PDF file")
+    
+    print(">>>>>", bbox)
+    prev_bbox = [float(coord) for coord in bbox.split(",")]
+    page_index = page_num - 1
+    if page_index < 0:
+        raise HTTPException(status_code=400, detail=f"Invalid page number: {page_num}")
 
-    with pdfplumber.open(io.BytesIO(pdf_bytes), pages=[page_num]) as pdf:
-        page = pdf.pages[0]
-        pix = page.to_image(resolution=dpi)
-        img = pix.original
+    try:
+        with pdfplumber.open(io.BytesIO(pdf_bytes), pages=[page_index]) as pdf:
+            if len(pdf.pages) == 0:
+                raise HTTPException(status_code=400, detail=f"Page number {page_num} out of range")
+            page = pdf.pages[0]
+            img1 = page.to_image(resolution=275).original
+            img2 = page.to_image(resolution=dpi).original
+            
+            scale_factor_x = img2.width / img1.width
+            scale_factor_y = img2.height / img1.height
+
+            x1, y1, x2, y2 = [coord * scale for coord, scale in zip(prev_bbox[:4], [scale_factor_x, scale_factor_y, scale_factor_x, scale_factor_y])]
+            
+            buffered = BytesIO()
+            img2.save(buffered, format="PNG")
+            img2_string = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+            return {
+                "page_num": page_num,
+                "dpi": dpi,
+                "m_bbox": [x1, y1, x2, y2],
+                "m_img": img2_string
+            }
+    except IndexError:
+        raise HTTPException(status_code=400, detail=f"Page number {page_num} out of range")
         
-        # Convert PIL image to base64
-        buffered = BytesIO()
-        img.save(buffered, format="PNG")
-        img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+@app.post("/extract")
+async def extract(
+    pdf_file: UploadFile = File(...),
+    data: str = Form(...),
+):
+    temp_dir = tempfile.mkdtemp()
+    with open("ext_bbox.txt", 'w') as f:
+        f.write(str(data))
+    print("data: ", data)
+
+    try:
+        pdf_path = os.path.join(temp_dir, pdf_file.filename)
+        contents = await pdf_file.read()
+        with open(pdf_path, 'wb') as f:
+            f.write(contents)
         
-        return {"image": img_str, "dpi": dpi, "height": img.height, "width": img.width}
+        data = json.loads(data)
+        print(data)
+        excel_files_info = []
+        
+        with pdfplumber.open(pdf_path) as pdf:
+            for page in data:
+                pg_no = page['page_num']
+                category = page['category']
+                dpi = page['dpi']
 
+                page_index = pg_no - 1  # Adjust for zero-based indexing
+                if page_index < 0 or page_index >= len(pdf.pages):
+                    raise HTTPException(status_code=400, detail=f"Page number {pg_no} out of range for the provided PDF")
+                pl_page = pdf.pages[page_index]
+                pg_image = pl_page.to_image(resolution=dpi).original
+                # img_path = os.path.join(temp_dir, "img.png")
+                # pg_image.save(img_path)
+                # pg_image = Image.open(img_path)#hanged
+        
+                if category in ['A3', 'Scanned']:
+                    excel_file = os.path.join(temp_dir, f'{os.path.splitext(pdf_file.filename)[0]}_page-{pg_no}.xlsx')
+                    with pd.ExcelWriter(excel_file, engine='openpyxl') as writer:
+                        start_row = 0
+                        tbl_count = 0
+                        for tables in page['tables']:
+                            class_id = tables['class_id']
+                            bbox = tables['bbox']
+                            print(">>>>bbox: ", bbox)
+                            cropped_img = pg_image.crop(bbox)
+                            if class_id == 2:
+                                cropped_img = cropped_img.rotate(270, expand=True)
+                            img_buffer = io.BytesIO()
+                            cropped_img.save(f"{tbl_count}-test.png")
+                            cropped_img.save(img_buffer, format="PNG")
+                            img_base64 = base64.b64encode(img_buffer.getvalue()).decode('utf-8')
+                            
+                            html_table_content, usage = agent.extract_table(img_base64)
+        
+                            for gen_table in html_table_content:
+                                gen_table = "<table" + gen_table
+                                tbl_count += 1
+        
+                                html_file = os.path.join(temp_dir, f'file-{pdf_file.filename[:-4]}-page-{pg_no}-table-{tbl_count}.html')
+                                with open(html_file, 'w', encoding='utf-8') as file:
+                                    file.write(gen_table)
+                                        
+                                excel_file_per_table = os.path.join(temp_dir, f'file-{pdf_file.filename[:-4]}-page-{pg_no}-table-{tbl_count}.xlsx')
+                                convert_htm_to_excel(html_file, excel_file_per_table)
+        
+                                #table_df = pd.read_excel(excel_file_per_table)
+                                with pd.ExcelFile(excel_file_per_table) as xls:#changed
+                                    table_df = pd.read_excel(xls)
+        
+                                if isinstance(table_df.columns, pd.MultiIndex):
+                                    table_df.columns = [' '.join(col).strip() for col in table_df.columns.values]
+        
+                                table_df.to_excel(writer, index=False, header=True, startrow=start_row, sheet_name='Page Tables')
+                                        
+                                start_row += len(table_df) + 3
+                                excel_files_info.append({
+                                    'excel_file': excel_file,
+                                    'page_num': pg_no,
+                                    'table_num': tbl_count,
+                                    'image': pl_page.to_image(resolution=95).original.rotate(270, expand=True) if class_id == 2 else pl_page.to_image(resolution=95)
+                                })
+                        
+                elif category == 'Word':
+                    excel_file = os.path.join(temp_dir, f'{os.path.splitext(pdf_file.filename)[0]}_page-{pg_no}.xlsx')
+                    pl_page = pdf.pages[pg_no-1]
 
-# @app.post("/extract")
-# async def extract(
-#     pdf_file: UploadFile = File(...),
-#     page_details: List[int] = Form(...),
-# )
+                    extracted_tables = pl_page.extract_tables()
+                    with pd.ExcelWriter(excel_file, engine='openpyxl') as writer:
+                        start_row = 0
+                        tbl_count = 0
+                        for tbl_no, table in enumerate(extracted_tables):
+                            tbl_count += 1
+                            table_df = pd.DataFrame(table[1:], columns=table[0])
+        
+                            if isinstance(table_df.columns, pd.MultiIndex):
+                                table_df.columns = [' '.join(col).strip() for col in table_df.columns.values]
+        
+                            table_df.to_excel(writer, index=False, header=True, startrow=start_row, sheet_name='Page Tables')
+                                    
+                            start_row += len(table_df) + 3
+                                    
+                            if not table_df.empty:
+                                excel_files_info.append({
+                                    'excel_file': excel_file,
+                                    'page_num': pg_no,
+                                    'table_num': tbl_count,
+                                    "image": pl_page.to_image(resolution=95)
+                                })
+                        
+                else:
+                    print(f"Skipping page {pg_no} with invalid category '{category}'.")
+                
+        if excel_files_info:
+            combined_excel_path = os.path.join(temp_dir, f'{pdf_file.filename[:-4]}_combined.xlsx')
+            img_added_pg_no = []
+            with pd.ExcelWriter(combined_excel_path, engine='openpyxl') as writer:
+                for file_info in excel_files_info:
+                    #df = pd.read_excel(file_info['excel_file'])
+                    with pd.ExcelFile(file_info['excel_file']) as xls:#changed
+                        df = pd.read_excel(xls)
+                    sheet_name = f'Page_{file_info["page_num"]}'
+                    df.to_excel(writer, sheet_name=sheet_name, index=False)
+        
+                    if file_info['page_num'] not in img_added_pg_no:
+                        img_added_pg_no.append(file_info['page_num'])
+                        workbook = writer.book
+                        worksheet = workbook[sheet_name]
+        
+                        img_buffer = io.BytesIO()
+                        file_info['image'].save(img_buffer, format="PNG")
+                        img_buffer.seek(0)
+                        img_for_excel = ExcelImage(img_buffer)
+        
+                        worksheet.add_image(img_for_excel, "R1")
+        else:
+            # return with 400 status code with message saying no tables found
+            shutil.rmtree(temp_dir)
+            raise HTTPException(status_code=400, detail="no tables")
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+        def cleanup():
+            shutil.rmtree(temp_dir)
+
+        return FileResponse(
+            path=combined_excel_path,
+            filename=f'{pdf_file.filename[:-4]}_combined.xlsx',
+            media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            background=BackgroundTask(cleanup)
+        )
+
+    except Exception as e:
+        shutil.rmtree(temp_dir)
+        raise e
